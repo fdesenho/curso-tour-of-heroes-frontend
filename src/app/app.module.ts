9d30140b264ec0e56b26e5b8de50957825afb535
fdesenho <fdesenho@hotmail.com>
2022-11-11 13:57:48 -0300

    Detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HerodetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HerodetailComponent,
+
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mock.heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name:' Wolverine'},
+  {id: 2, name:' Spiderman'},
+  {id: 3, name:' Storm'},
+  {id: 4, name:' Brain'},
+  {id: 5, name:' Batman'}
+]
