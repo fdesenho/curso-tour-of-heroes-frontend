1b51e9006bc200bd7d64825a9502a883b4d936ed
fdesenho <fdesenho@hotmail.com>
2023-01-11 16:24:08 -0300

    Base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://toh-json-server-api.herokuapp.com/api',
+  baseUrl: '/api'
+ // baseUrl: 'https://toh-json-server-api.herokuapp.com/api',
 };
